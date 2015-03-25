bbaf45358490f3b9ef1721baa3465784b6b087a0
pandaoknight@iZ25xor03qdZ <pandaoknight@iZ25xor03qdZ.com>
2015-03-25 15:28:02 +0800

    实现了'empty($var)', '!isset($var)'的对比。

--- 01_True_False_Empty/main.php
+++ 01_True_False_Empty/main.php
@@ -1,8 +1,13 @@
 <?php
 require_once(__DIR__ . '/../lib/php-cli-tools/vendor/autoload.php');
 
-$exps = ["True == %s", "False == %s", "empty(%s)", "NULL == %s"];
-$exps_name = ["True == ", "False == ", "empty()", "NULL == "];  // %s 会引起的 php-cli-tools 画表格时的Bug。
+$exps = ["True == %s", "False == %s", "empty(%s)", "isset(%s)", "NULL == %s"];
+$exps_name = array_map(function($s){return str_replace("%s", "", $s);}, $exps);// %s 会引起的 php-cli-tools 画表格时的Bug。
+
+#$var_exps = ['empty($var)', 'isset($var)'];
+$var_exps = ['empty($var)', '!isset($var)'];
+$var_exps_name = array_map(function($s){return str_replace("%s", "", $s);}, $var_exps);
+
 $cases = [
   '1',
   '0',
@@ -16,12 +21,18 @@ $cases = [
   '',
   'NULL',
   '"NULL"',
+  'null',
+  'Null',
+  'nUll',
 ];
+
 
 $rows = [];
 $i = 0;
 foreach($cases as $case){
   $rows[$i] = [$case];
+
+  # 我们发现empty()，isset()不能处理非变量的情况。会直接报语法错。
   foreach($exps as $exp){
     $cmd =  "return var_export( " . sprintf($exp, $case) . ", True );";
     $val = eval($cmd);
@@ -29,6 +40,16 @@ foreach($cases as $case){
 
     $rows[$i][] = $val;
   }
+
+  #增加专门测试empty()，isset()行为的测试。
+  foreach($var_exps as $exp){
+    $cmd =  '$var=' . $case . ';' . "return var_export( " . $exp . ", True );";
+    $val = eval($cmd);
+    print $cmd . " : " . $val . "\n";
+
+    $rows[$i][] = $val;
+  }
+
   $i++;
 }
 
@@ -36,7 +57,7 @@ foreach($cases as $case){
 // 顶部Header是测试的表达式
 // 第一列是测试用例的值
 // 中间部分则是boolean(true)和boolean(false)
-$headers = $exps_name;
+$headers = array_merge($exps_name, $var_exps_name);
 array_unshift($headers, "Cases");
 
 $t = new \cli\Table();
